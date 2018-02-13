0970ace5d010bfb488844bf6c1ea67b37aaa32f3
Nagy Zsolt <32664284+nagyzsolt97@users.noreply.github.com>
2018-02-13 10:48:35 +0100

    Update ansible-install.sh

--- ansible-install.sh
+++ ansible-install.sh
@@ -4,3 +4,4 @@ sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible
+sudo ansible-playbook playbook.yml -i hosts
